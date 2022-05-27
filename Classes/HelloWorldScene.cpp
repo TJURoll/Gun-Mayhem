0b7d0a88f5fd19316c3d195780e652818c834687
yixin <2152058@tongji.edu.cn>
2022-05-27 21:46:35 +0800

    修改了窗口大小并适配

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -3,14 +3,6 @@
 USING_NS_CC;
 
 
-// Print useful error message instead of segfaulting when files are not there.
-static void problemLoading(const char* filename)
-{
-	printf("Error while loading: %s\n", filename);
-	printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
-}
-
-// on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
 	if (!Scene::init())
@@ -24,7 +16,7 @@ bool HelloWorld::init()
 	auto sprite = Sprite::create("logo.jpeg");
 	if (sprite == nullptr)
 	{
-		problemLoading("'logo.jpeg'");
+		return false;
 	}
 	else
 	{
